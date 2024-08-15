6a13dc0784142a7ffdfcf9893a599035ef4e9d31
Cornelius Corkery <cornelius.corkery@gmail.com>
2024-08-15 11:14:42 -0700

    Updated project files and on a cursor journey

---
+++ colors.js
@@ -0,0 +1,16 @@
+function getColor(colorScheme, shade) {
+  // Access your color object here (assuming it's defined elsewhere)
+  return 'cumin': {
+        '50': '#fdf7ef',
+        '100': '#fbedd9',
+        '200': '#f7d8b1',
+        '300': '#f1be80',
+        '400': '#eb994c',
+        '500': '#e57d2a',
+        '600': '#d7641f',
+        '700': '#b24d1c',
+        '800': '#8b3c1d',
+        '900': '#73341b',
+        '950': '#3e190c',
+    },}
+
---
+++ data.js
@@ -0,0 +1,19 @@
+const cats = [
+  {
+    image: "./images/neighbor-cats/treecat.JPG",
+    altText: "treecat",
+    title: "Treecat",
+    image2: "./images/neighbor-cats/sushi.JPG",
+    altText2: "sushi",
+    description: "Sushi (RIP)"
+  },
+  {
+    image: "./images/neighbor-cats/redcollarcat.JPG",
+    altText: "redcollarcat",
+    title: "Red Collar Cat",
+    image2: "./images/neighbor-cats/wolf.jpg",
+    altText2: "wolf",
+    description: "Wolf (honorary neighbor cat)"
+  }
+];
+
