ae43e0895bb37d451d67e7bb0f0f1f73e855d46f
Cornelius Corkery <cornelius.corkery@gmail.com>
2024-08-15 22:17:29 -0700

    Fixed Gulp tasks and project structure

---
+++ my-github-pages-site/data.js
@@ -0,0 +1,39 @@
+const cats = [
+  {
+    name: "Treecat",
+    image: "./images/neighbor-cats/treecat.JPG",
+    description: "A cat that loves climbing trees"
+  },
+  {
+    name: "Sushi",
+    image: "./images/neighbor-cats/sushi.JPG",
+    description: "Sushi (RIP) - A beloved neighborhood cat"
+  },
+  {
+    name: "Red Collar Cat",
+    image: "./images/neighbor-cats/redcollarcat.JPG",
+    description: "Known for its distinctive red collar"
+  },
+  {
+    name: "Wolf",
+    image: "./images/neighbor-cats/wolf.jpg",
+    description: "Wolf (honorary neighbor cat)"
+  },
+  {
+    name: "Albert",
+    image: "./images/neighbor-cats/albert.jpg",
+    description: "A scrappy fighter"
+  },
+  {
+    name: "Teddy",
+    image: "./images/neighbor-cats/teddy.jpg",
+    description: "A well-traveled cat"
+  },
+  {
+    name: "Not Teddy",
+    image: "./images/neighbor-cats/not_teddy.jpg",
+    description: "Not Teddy"
+  }
+];
+
+module.exports = { cats };
